3f601952fb1464801eea96a12084a424b58cc4f2
Sandal <sandalmaan28@gmail.com>
2023-07-14 14:45:17 +0100

    Message project created

---
+++ Message.js
@@ -0,0 +1,26 @@
+let randnum= Math.floor(Math.random()*7);
+console.log(randnum);
+let weekdays=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+switch(randnum){
+    case 0:
+        console.log("you Will get Rich on "+weekdays[randnum]);
+        break;
+    case 1:
+        console.log("you Will get Rich on "+weekdays[randnum]);
+        break;
+    case 2:
+        console.log("you Will get Rich on "+weekdays[randnum]);
+        break;
+    case 3:
+        console.log("you Will get Rich on "+weekdays[randnum]);
+        break;
+        case 4:
+            console.log("you Will get Rich on "+weekdays[randnum]);
+            break;
+            case 5:
+                console.log("you Will get Rich on "+weekdays[randnum]);
+                break;
+                case 6:
+                    console.log("you Will get Rich on "+weekdays[randnum]);
+                    break;
+}
